2265e9a41e76aef9aeb871dd1f50865b1217b882
DevYemi <silasadegoke331@gmail.com>
2024-09-18 10:04:52 +0100

    Refactored the authentication UI

--- src/Register/index.tsx
+++ src/Register/index.tsx
@@ -9,11 +9,11 @@ import Alerts from "../global components/Alert";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import s from "./register.module.scss";
 import clsx from "clsx";
+import Line from "../../public/assets/images/Line 2.png";
 type Inputs = {
   email: string;
   password: string;
 };
-
 function Register() {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState<boolean>(false);
@@ -33,7 +33,7 @@ function Register() {
     try {
       await CreateUserWithEmailAndPassword(data.email, data.password);
       setAlert({ message: "Registration successful!", type: "success" });
-      navigate("/login")
+      navigate("/login");
     } catch (error: any) {
       const errorMessage = error.message || "Registration failed";
 
@@ -53,30 +53,36 @@ function Register() {
   };
   return (
     <div className={clsx(s.wrapper)}>
-        <h3>Create your account</h3>
-      <button onClick={googleSigin}>Sigin With google</button>
+      <h3>Create an account</h3>
       <form onSubmit={handleSubmit(onSubmit)}>
         {alert && <Alerts message={alert.message} type={alert.type} />}
         <span>
-          <p>Email</p>
           <input
             type="email"
+            placeholder="Email Address"
             {...register("email", { required: "Email is required" })}
           />
           {errors.email && <span>{errors.email.message}</span>}
         </span>
 
         <span>
-          <p>Password</p>
           <span className={clsx(s.passwordIcon)}>
             {showPassword ? (
-              <EyeOutlined onClick={() => setShowPassword(false)} />
+              <EyeOutlined
+                width={150}
+                className={clsx(s.icon)}
+                onClick={() => setShowPassword(false)}
+              />
             ) : (
-              <EyeInvisibleOutlined onClick={() => setShowPassword(true)} />
+              <EyeInvisibleOutlined
+                className={clsx(s.icon)}
+                onClick={() => setShowPassword(true)}
+              />
             )}
           </span>
           <input
             type={showPassword ? "text" : "password"}
+            placeholder="Password"
             {...register("password", {
               required: "Password is required",
               pattern: {
@@ -89,13 +95,39 @@ function Register() {
               },
             })}
           />
-          {errors.password && <span style={{color:"red"}}>{errors.password.message}</span>}
+          {errors.password && (
+            <span style={{ color: "red" }}>{errors.password.message}</span>
+          )}
         </span>
 
-        <button className={clsx(s.login)}>Register</button>
+        <button className={clsx(s.login)}>Continue</button>
       </form>
-      <span>
-        Don't have an account? <Link to="/login">Register your account</Link>
+      <span className={s.account}>
+        Already have an account? <Link to="/login">Login</Link>
+      </span>
+      <span className={clsx(s.or)}>
+        <img
+          width={"150px"}
+          height={"1px"}
+          src={`${process.env.PUBLIC_URL}/assets/images/Line 2.png`}
+          alt="Line 2"
+        />
+        <p>Or</p>
+        <img
+          width={"150px"}
+          height={"1px"}
+          src={`${process.env.PUBLIC_URL}/assets/images/Line 2.png`}
+          alt="Line 2"
+        />
+      </span>
+      <span className={clsx(s.google)} onClick={googleSigin}>
+        <img
+          width={"32px"}
+          height={"38px"}
+          src={`${process.env.PUBLIC_URL}/assets/images/Google.png`}
+          alt="Line 2"
+        />
+        <p> Continue with Google</p>
       </span>
     </div>
   );
